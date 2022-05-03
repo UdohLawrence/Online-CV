c5963773392ba43ea11b98ccafb5464aca5ca54f
UdohLawrence <lawrenceaudoh@gmail.com>
2022-05-03 21:33:49 +0100

    Added the index.js declarations
    The rest will come later.

---
+++ JavaScript/index.js
@@ -0,0 +1,4 @@
+const sections = document.querySelectorAll(".section");
+const sectionButtons = document.querySelectorAll(".controls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelectorAll(".main-content");
